a228072ea5499b0f5d0a4ff07602fdcac82a52d7
Matt Christiansen <matt@nikore.net>
2014-05-18 01:37:45 -0700

    added a Java smoke test example

---
+++ src/test/java/net/nikore/etcd/JavaEtcdClientSmokeTest.java
@@ -0,0 +1,25 @@
+package net.nikore.etcd;
+
+import java.util.concurrent.TimeUnit;
+
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+public class JavaEtcdClientSmokeTest {
+  public static void main(String ... args) throws Exception {
+    EtcdClient client = new EtcdClient("http://localhost:4001");
+
+    Future<EtcdJsonProtocol.EtcdResponse> setResponse = client.setKey("test1", "testValue");
+
+    EtcdJsonProtocol.EtcdResponse etcdSetResponse = Await.result(setResponse, Duration.create(5, TimeUnit.SECONDS));
+
+    System.out.println(etcdSetResponse);
+
+    Future<EtcdJsonProtocol.EtcdResponse> getResponse = client.getKey("test1");
+
+    EtcdJsonProtocol.EtcdResponse etcdResponse = Await.result(getResponse, Duration.create(5, TimeUnit.SECONDS));
+
+    System.out.println(etcdResponse);
+  }
+}
